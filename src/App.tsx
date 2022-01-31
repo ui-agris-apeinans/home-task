bd4cc8f6c0370f184196157d288c59fb0c7390ad
Agris Apeinans <agrisapeinans@Agriss-MacBook-Air.local>
2022-01-31 10:34:01 +0200

    add api service

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography, Container } from '@mui/material';
 
 import './App.css';
+import { postsLink } from './constants';
+import callApi from './services/api';
 
 function App() {
+  useEffect(() => {
+    callApi(postsLink).then(res => {
+      console.log(res)
+
+    })
+  }, [])
   return (
     <div className="App">
+      <Container>
+        <Typography variant="h4" align="center" paddingTop="12px">Posts</Typography>
+      </Container>
     </div>
   );
 }
