16e04bb4adc5975d3117f59e980888ada2ee6024
Agris Apeinans <agrisapeinans@Agriss-MacBook-Air.local>
2022-02-02 14:12:43 +0200

    init Posts test, minor refactor

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders app with title', () => {
+test('renders App with correct title', () => {
   render(<App />);
   const title = screen.getByText(/Posts Table/i);
   expect(title).toBeInTheDocument();
